b7ed846d2fb686eeb8dc0410324ce7c3b9e85bdf
gusdn0413 <gusdn0413@naver.com>
2026-05-01 19:06:06 +0900

    클래스 명 변경 ApiEndpoint -> ApiCall

--- src/main/java/com/hyun/eventpipeline/provider/ApiCall.java
+++ src/main/java/com/hyun/eventpipeline/provider/ApiCall.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import org.springframework.http.HttpMethod;
 
 @Getter
-public enum ApiEndpoint {
+public enum ApiCall {
 
     AUTH_LOGIN("로그인", HttpMethod.POST, "/api/auth/login"),
     PRODUCT_SEARCH("상품 검색", HttpMethod.GET, "/api/products/search"),
@@ -13,11 +13,11 @@ public enum ApiEndpoint {
 
     private final String description;
     private final HttpMethod method;
-    private final String pathTemplate;
+    private final String endpoint;
 
-    ApiEndpoint(String description, HttpMethod method, String pathTemplate) {
+    ApiCall(String description, HttpMethod method, String endpoint) {
         this.description = description;
         this.method = method;
-        this.pathTemplate = pathTemplate;
+        this.endpoint = endpoint;
     }
 }
