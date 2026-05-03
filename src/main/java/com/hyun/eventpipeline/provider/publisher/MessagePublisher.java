173c1409e57e54450ba0d1a12b13b4d8915f50fd
gusdn0413 <gusdn0413@naver.com>
2026-05-03 12:26:02 +0900

    주석 수정

--- src/main/java/com/hyun/eventpipeline/provider/publisher/MessagePublisher.java
+++ src/main/java/com/hyun/eventpipeline/provider/publisher/MessagePublisher.java
@@ -1,9 +1,6 @@
 package com.hyun.eventpipeline.provider.publisher;
 
-/**
- * 메시지 브로커 추상화.
- * 구현체로 NATS / Spring ApplicationEvent 두 가지를 두고 messaging.broker 프로퍼티로 스위칭.
- */
+// 메시지 브로커 추상화 (NATS, EventListener)
 public interface MessagePublisher {
 
     void publish(String message);
